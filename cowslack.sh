4efdd897c93b9937bfb3eed0b3f1726090d976f6
M. Spiceyapple <m.spiceyapple@gmail.com>
2018-02-09 18:11:59 +1100

    added PATH

--- cowslack.sh
+++ cowslack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\",\"username\": \"cowslack\", \"text\": \"$(fortune) :cow2:\", \"icon_emoji\": \":cow:\"}" https://hooks.slack.com/YOUR/WEBHOOK/URL
+PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/SPICEYAPPLE/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#development\",\"username\": \"cowslack\", \"text\": \" $(fortune) :cow2:\", \"icon_emoji\": \":cow:\"}" https://hooks.slack.com/services/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
